6e5a2299bba3b44b0af61e4de322621e3888e7e2
ryanguyen206 <ryanninja595@gmail.com>
2024-01-02 18:15:53 -0800

    add username and genre to serializert

--- backend/book/admin.py
+++ backend/book/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import BookReview
+from .models import BookReview, Genre
 # Register your models here.
 
 admin.site.register(BookReview)
+admin.site.register(Genre)
--- backend/book/serializer.py
+++ backend/book/serializer.py
@@ -1,15 +1,25 @@
 from rest_framework import serializers
-from .models import BookReview
+from .models import BookReview, Genre
+
 
 class BookReviewSerializer(serializers.ModelSerializer):
+    user_name = serializers.CharField(source='creator.username', read_only=True)
+    genre_name = serializers.CharField(source='genre.genre', read_only=True)
+
     class Meta:
         model = BookReview
         fields = [
+            'pk',
             'creator',
             'genre',
+            'genre_name',
+            'user_name',
             'participants',
             'book_title',
             'review_title',
             'body',
             'created'
         ]
+        
+        
+ 
