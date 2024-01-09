0c88da9600ee4fe81a8a12dc9077adef5c32272e
ryanguyen206 <ryanninja595@gmail.com>
2024-01-09 15:34:05 -0800

    dont use flex

--- frontend/src/components/HomePage/BookFeed.jsx
+++ frontend/src/components/HomePage/BookFeed.jsx
@@ -20,11 +20,11 @@ const BookFeed = ({currentGenre}) => {
   return (
     
       filteredReviews && filteredReviews.length === 0 ? <p className='text-center my-20 text-stone-500 text-2xl uppercase'>No book reviews</p> :
-      <div className='flex flex-wrap gap-8 mt-20 md:grid md:grid-cols-2 md:grid-rows-2 text-stone-500 font-medium'>
+      <div className=' gap-8 mt-20 grid md:grid-cols-2 md:grid-rows-2 text-stone-500 font-medium'>
         {filteredReviews?.map(review => (
-          <div key={review.id} className='w-full flex-auto'>
-             <SingleBookFeed review={review}/>
-          </div>
+        
+             <SingleBookFeed key={review.id} review={review}/>
+      
      
         ))}
       </div>
